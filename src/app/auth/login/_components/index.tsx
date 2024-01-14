b4e0bd0c8915a7fa3f0a90b321f4230f4b0c2721
Shelly <shellybhattarai@gmail.com>
2024-01-14 19:09:05 +0545

    modify login component according to new input component

--- src/app/auth/login/_components/index.tsx
+++ src/app/auth/login/_components/index.tsx
@@ -1,25 +1,36 @@
-import Button from "@/components/Button";
-import Input from "@/components/Input";
+import Button from '@/components/Button';
+import Input from '@/components/Input';
+import PersonIcon from '@mui/icons-material/Person';
+import LockIcon from '@mui/icons-material/Lock';
 
 const LoginPage = () => {
-  return (
-    <div>
-      <div className="title">Sign In</div>
+    return (
+        <div>
+            <div className="title">Sign In</div>
 
-      <div className="input-form">
-        <Input variant="standard" label="Username" />
-        <Input variant="standard" label="Password" type="password" />
+            <div className="input-form">
+                <Input
+                    variant="standard"
+                    label="Username"
+                    StartIcon={PersonIcon}
+                />
+                <Input
+                    variant="standard"
+                    label="Password"
+                    type="password"
+                    StartIcon={LockIcon}
+                />
 
-        <Button variant="contained">Log In</Button>
-      </div>
-      <div>
-        <span>Forgot Password?</span>
-      </div>
-      <div>
-        <Button variant="text">create an account</Button>
-      </div>
-    </div>
-  );
+                <Button variant="contained">Log In</Button>
+            </div>
+            <div>
+                <span>Forgot Password?</span>
+            </div>
+            <div>
+                <Button variant="text">create an account</Button>
+            </div>
+        </div>
+    );
 };
 
 export default LoginPage;
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,5 @@
-import LoginPage from "./_components";
+import LoginPage from './_components';
 
 export default function Login() {
-return <LoginPage />    
-} 
+    return <LoginPage />;
+}
