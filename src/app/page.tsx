d3556fafa8fe01d7d8b41372c61cf7974d1c33a7
Shelly <shellybhattarai@gmail.com>
2024-03-07 14:40:35 +0545

    use card component in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Card from '@/components/Card'
+import styles from './page.module.css';
+
+import Card from '@/components/Card';
+import { Product } from '@/utils/schema';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Card />
-    </main>
-  )
+    const products: Array<Product> = [
+        {
+            id: 1,
+            imageUrl:
+                'https://mode23nepal.com/image/catalog/Product/Feb%20sweatshirt%20+%20Zip/DSC00356.jpg',
+            productName: 'Floral Dress',
+            productPrice: '2300',
+        },
+    ];
+
+    return (
+        <main className={styles.main}>
+            {products.map(product => {
+                return <Card key={product.id} product={product} />;
+            })}
+        </main>
+    );
 }
