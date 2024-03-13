bf0cd879e7e3a27921c70ca7768228bcd1b9a232
Shelly <shellybhattarai@gmail.com>
2024-03-13 12:09:52 +0545

    create new password page for forgot password

---
+++ src/app/(auth)/forgot-password/new-password/_components/index.tsx
@@ -0,0 +1,114 @@
+'use client';
+
+import React from 'react';
+
+import { useRouter } from 'next/navigation';
+import { useDispatch } from 'react-redux';
+
+import LockIcon from '@mui/icons-material/Lock';
+import Grid from '@mui/material/Grid';
+import * as Yup from 'yup';
+
+import FormInput from '@/components/Form/FormInput';
+import FormButton from '@/components/Form/FormButton';
+import CustomForm from '@/components/Form';
+import { login } from '@/redux/features/userSlice';
+import { openToast } from '@/redux/features/toastSlice';
+import URLS from '@/utils/urls';
+
+const validationSchema = Yup.object({
+    new_password: Yup.string().required('Password is required'),
+    confirm_new_password: Yup.string()
+        .required('Confirm password is required')
+        .oneOf([Yup.ref('new_password')], 'Passwords must match'),
+});
+
+const NewPasswordPage = () => {
+    const router = useRouter();
+    const dispatch = useDispatch();
+
+    const initialValues = {
+        new_password: '',
+        confirm_new_password: '',
+    };
+
+    const handleSubmit = async (values: any, actions: any) => {
+        console.log('Values: ', values);
+        // const response = await fetch(URLS.USER_LOGIN_URL, {
+        //     method: 'POST',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify(values),
+        // });
+
+        // if (response.ok) {
+        //     const data = await response.json();
+        //     dispatch(login(data));
+        //     dispatch(
+        //         openToast({
+        //             message: 'User Login Successful',
+        //             severity: 'success',
+        //         }),
+        //     );
+
+        //     actions.setSubmitting(false);
+        //     actions.resetForm(initialValues);
+        //     router.push('/');
+        // } else {
+        //     dispatch(
+        //         openToast({ message: 'User Login Failed', severity: 'error' }),
+        //     );
+        //     actions.setSubmitting(false);
+        // }
+    };
+
+    return (
+        <Grid
+            container
+            item
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+        >
+            <CustomForm
+                title="New Password"
+                initialValues={initialValues}
+                submitHandler={handleSubmit}
+                validationSchema={validationSchema}
+                showBoxShadow
+            >
+                <Grid
+                    container
+                    item
+                    sx={{
+                        width: '400px',
+                    }}
+                >
+                    <Grid container item sx={{ margin: 2 }}>
+                        <FormInput
+                            variant="standard"
+                            label="New Password"
+                            type="password"
+                            name="new_password"
+                            StartIcon={LockIcon}
+                        />
+                        <FormInput
+                            variant="standard"
+                            label="Confirm New Password"
+                            type="password"
+                            name="confirm_new_password"
+                            StartIcon={LockIcon}
+                        />
+
+                        <FormButton
+                            variant="contained"
+                            type="submit"
+                            label="Change Password"
+                        />
+                    </Grid>
+                </Grid>
+            </CustomForm>
+        </Grid>
+    );
+};
+
+export default NewPasswordPage;
