7fc48097455b5a65b4ed5bc57bebcb4ef1f90380
Shelly <shellybhattarai@gmail.com>
2024-03-24 15:32:16 +0545

    add more state to user slice

--- src/redux/features/userSlice.ts
+++ src/redux/features/userSlice.ts
@@ -1,51 +1,75 @@
-'use client';
+"use client";
 
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+
+interface UserData {
+  id: number;
+  email: string;
+  roles: Array<String>;
+}
 
 export interface UserDetails {
-    access_token: string;
-    refresh_token: string;
-    loggedIn: boolean;
+  access_token: string;
+  refresh_token: string;
+  loggedIn: boolean;
+  userID: Number;
+  userEmail: string;
+  isAdmin: boolean;
+  isShopAdmin: boolean;
+  isCustomer: boolean;
 
-    forgotPasswordEmail: string;
+  forgotPasswordEmail: string;
 }
 
 const initialState: UserDetails = {
-    access_token: '',
-    refresh_token: '',
-    loggedIn: false,
+  access_token: "",
+  refresh_token: "",
+  loggedIn: false,
+  userID: 0,
+  userEmail: "",
+  isAdmin: false,
+  isShopAdmin: false,
+  isCustomer: false,
 
-    forgotPasswordEmail: '',
+  forgotPasswordEmail: "",
 };
 
 export const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        login: (state, action) => {
-            state.loggedIn = true;
-            state.access_token = action.payload.access;
-            state.refresh_token = action.payload.refresh;
-        },
-        logout: state => {
-            state.loggedIn = false;
-            state.access_token = '';
-            state.refresh_token = '';
-        },
-        setForgotPasswordEmail: (state, action) => {
-            state.forgotPasswordEmail = action.payload.email;
-        },
-        removeForgotPasswordEmail: state => {
-            state.forgotPasswordEmail = '';
-        },
+  name: "user",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.loggedIn = true;
+      state.access_token = action.payload.access;
+      state.refresh_token = action.payload.refresh;
+
+      const userData: UserData = JSON.parse(action.payload.user);
+      if (userData) {
+        state.userID = userData.id;
+        state.userEmail = userData.email;
+
+        state.isAdmin = userData.roles.includes("Admin");
+        state.isShopAdmin = userData.roles.includes("ShopAdmin");
+        state.isCustomer = userData.roles.includes("Customer");
+      }
     },
+    logout: (state) => {
+      return initialState;
+    },
+    setForgotPasswordEmail: (state, action) => {
+      state.forgotPasswordEmail = action.payload.email;
+    },
+    removeForgotPasswordEmail: (state) => {
+      state.forgotPasswordEmail = "";
+    },
+  },
 });
 
 export const {
-    login,
-    logout,
-    setForgotPasswordEmail,
-    removeForgotPasswordEmail,
+  login,
+  logout,
+  setForgotPasswordEmail,
+  removeForgotPasswordEmail,
 } = userSlice.actions;
 
 export default userSlice.reducer;
