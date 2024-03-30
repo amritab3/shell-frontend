0900d4c8de76d24522d7877ed3a4e8f76aed6478
Shelly <shellybhattarai@gmail.com>
2024-03-30 23:47:59 +0545

    change men page

--- src/app/(root)/products/instore/men/_components/index.tsx
+++ src/app/(root)/products/instore/men/_components/index.tsx
@@ -1,11 +1,22 @@
 'use client';
 
 import React from 'react';
-
-import NotFound from '@/components/404';
+import Grid from '@mui/material/Grid';
 
 const MenProducts = () => {
-    return <NotFound pageName="MEN" />;
+    return(
+       <Grid container item justifyContent="center" gap={2} xs={12}>
+        <Grid item xs={12}> Breadcrumbs </Grid>
+        <Grid item xs={12}> Title </Grid>
+        <Grid item xs={12}> Refine Search </Grid>
+        <Grid item xs={12}> Sorting </Grid>
+        <Grid item xs={12}> Products </Grid>
+        <Grid item xs={12}> Pages </Grid>
+        
+
+
+       </Grid>
+    );
 };
 
 export default MenProducts;
