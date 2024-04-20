2d0d0b3f73aa14c367e498343948f6c38885907c
Shelly <shellybhattarai@gmail.com>
2024-04-20 23:36:15 +0545

    add buttons

--- src/app/(root)/products/instore/[id]/_components/index.tsx
+++ src/app/(root)/products/instore/[id]/_components/index.tsx
@@ -4,14 +4,31 @@ import React, { useEffect } from "react";
 import { useParams } from "next/navigation";
 
 import NotFound from "@/components/404";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, TextField } from "@mui/material";
 import Rating from "@mui/material/Rating";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
 
 const itemInfoWidth: string = "100px";
 
 const ProductDetail = () => {
   const params = useParams();
   const [value, setValue] = React.useState<number | null>(0);
+  const [numberOfItems, setNumberOfItems] = React.useState(0);
+
+  const incrementItemCount = () => {
+    const newNumberOfItems = numberOfItems + 1;
+    setNumberOfItems(newNumberOfItems);
+  };
+
+  const decrementItemCount = () => {
+    let newNumberOfItems = numberOfItems - 1;
+    if (newNumberOfItems < 0) {
+      newNumberOfItems = 0;
+    }
+
+    setNumberOfItems(newNumberOfItems);
+  };
 
   const productDetails = {
     imageUrl:
@@ -23,11 +40,12 @@ const ProductDetail = () => {
     availability: true,
     numberOfStocks: "15",
     material: "Linen",
+    sizes: { S: 5, M: 5, L: 5, XL: 0 },
   };
 
   useEffect(() => {
     //fetch product data here
-    console.log(params.id);
+    console.log("Product ID: ", params.id);
   }, []);
 
   return (
@@ -95,17 +113,64 @@ const ProductDetail = () => {
             </Typography>
             <Typography>: {productDetails.material}</Typography>
           </Grid>
-          <Grid container item xs={6}>
+
+          <Grid container item xs={7}>
             <Typography
               variant="body1"
               gutterBottom
-              sx={{ width: itemInfoWidth }}
+              sx={{
+                width: itemInfoWidth,
+                fontSize: "1.1rem",
+                fontWeight: "bold",
+              }}
             >
-              Availability
+              Size
             </Typography>
-            <Typography>
-              : {productDetails.availability ? "In Stock" : "Out of Stock"}
-            </Typography>
+            <Grid container item xs={12}>
+              {Object.keys(productDetails.sizes).map((size: string) => {
+                const numberOfStocks: number = productDetails.sizes[size];
+                return (
+                  <Button
+                    key={size}
+                    disabled={
+                      !productDetails.availability || numberOfStocks <= 0
+                    }
+                    label={size}
+                    variant="outlined"
+                    sx={{ m: 1 }}
+                  />
+                );
+              })}
+            </Grid>
+          </Grid>
+
+          <Grid container item sx={{ mt: 2, mb: 2 }} gap={1}>
+            <Button
+              label="-"
+              variant="outlined"
+              sx={{ minHeight: 0, minWidth: 0 }}
+              onClick={decrementItemCount}
+            />
+            <TextField
+              variant="outlined"
+              value={numberOfItems}
+              size="small"
+              sx={{ width: "60px", "& input": { textAlign: "center" } }}
+            />
+            <Button
+              label="+"
+              variant="outlined"
+              sx={{ minHeight: 0, minWidth: 0 }}
+              onClick={incrementItemCount}
+            />
+          </Grid>
+
+          <Grid container item sx={{ mt: 2 }}>
+            {productDetails.availability ? (
+              <Button label="Add to Cart" variant="outlined" />
+            ) : (
+              <Button label="Out of Stock" disabled variant="outlined" />
+            )}
           </Grid>
         </Box>
       </Grid>
