02d6275e0d782699e8bbe304801fdfc472f7f0b3
Shelly <shellybhattarai@gmail.com>
2024-04-26 21:50:12 +0545

    remove unused imports and refactor file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,33 @@
-import ThemeRegistry from '@/theme/ThemeRegistry';
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import ThemeRegistry from "@/theme/ThemeRegistry";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-import Grid from '@mui/material/Grid';
+import { ReduxProvider } from "@/redux/provider";
+import Toast from "@/components/Toast";
 
-import { ReduxProvider } from '@/redux/provider';
-import Toast from '@/components/Toast';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: 'ThreadSwap',
-    description: 'An eCommerce platform',
+  title: "ThreadSwap",
+  description: "An eCommerce platform",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>
-                <ReduxProvider>
-                    <ThemeRegistry>
-                        {children}
-                        <Toast />
-                    </ThemeRegistry>
-                </ReduxProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ReduxProvider>
+          <ThemeRegistry>
+            {children}
+            <Toast />
+          </ThemeRegistry>
+        </ReduxProvider>
+      </body>
+    </html>
+  );
 }
