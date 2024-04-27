660e5e0828a37fd13fc2701e124170297d2873a0
Shelly <shellybhattarai@gmail.com>
2024-04-27 11:04:20 +0545

    fix style in details page

--- src/app/(root)/products/instore/[id]/_components/index.tsx
+++ src/app/(root)/products/instore/[id]/_components/index.tsx
@@ -3,10 +3,17 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 
-import { Box, Grid, Typography, TextField, Breadcrumbs, Link } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Typography,
+  TextField,
+  Breadcrumbs,
+  Link,
+} from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import TrendingFlatIcon from "@mui/icons-material/TrendingFlat";
 import Rating from "@mui/material/Rating";
 import Button from "@/components/Button";
 import { Product } from "@/utils/schema";
@@ -61,7 +68,7 @@ const ProductDetail = () => {
 
   return (
     <Grid container item xs={12} sx={{ m: 4 }}>
-      <Grid container spacing={10}>
+      <Grid container item>
         <Grid item xs={4}>
           <Box
             component="img"
@@ -77,138 +84,161 @@ const ProductDetail = () => {
             src={product.images[0].image}
           />
         </Grid>
-        <Grid item xs={8}>
-          <Grid item xs={12} marginBottom={2}>
-            <Breadcrumbs separator="|" aria-label="breadcrumb">
-              <Link underline="hover" sx={{ display: "flex", alignItems: "center" }} color="inherit" href="/">
-                <HomeIcon color="primary" sx={{ mr: 0.5 }} fontSize="small" />
-              </Link>
-              <Typography sx={{ display: "flex", alignItems: "center" }} color="text.primary">
-                {product.name}
-              </Typography>
-            </Breadcrumbs>
+        <Grid
+          container
+          item
+          xs={8}
+          direction="column"
+          justifyContent="space-between"
+        >
+          <Grid container item spacing={1}>
+            <Grid item xs={12} sx={{ display: "flex", alignItems: "center" }}>
+              <Breadcrumbs separator="|" aria-label="breadcrumb">
+                <Link
+                  underline="hover"
+                  sx={{ display: "flex", alignItems: "center" }}
+                  color="inherit"
+                  href="/"
+                >
+                  <HomeIcon color="primary" sx={{ mr: 0.5 }} fontSize="small" />
+                </Link>
+                <Typography
+                  sx={{ display: "flex", alignItems: "center" }}
+                  color="text.primary"
+                >
+                  {product.name}
+                </Typography>
+              </Breadcrumbs>
+            </Grid>
+            <Grid item sx={{ display: "flex", alignItems: "center" }}>
+              <Rating
+                name="product-rating"
+                value={ratingValue}
+                onChange={(event, newValue) => {
+                  setRatingValue(newValue);
+                }}
+              />
+            </Grid>
           </Grid>
-          <Rating
-            name="product-rating"
-            value={ratingValue}
-            onChange={(event, newValue) => {
-              setRatingValue(newValue);
-            }}
-          />
-          <Box sx={{ width: "100%", maxWidth: 500, mt: 5 }}>
+
+          <Grid item>
             <Typography variant="h6" gutterBottom>
               {product.name}
             </Typography>
             <Typography variant="h6" gutterBottom>
               Rs. {product.price}
             </Typography>
-            <Grid container item xs={6}>
-              <Typography
-                variant="body1"
-                gutterBottom
-                sx={{ width: itemInfoWidth }}
-              >
-                Style
-              </Typography>
-              <Typography>: {product.style}</Typography>
-            </Grid>
-            <Grid container item xs={6}>
-              <Typography
-                variant="body1"
-                gutterBottom
-                sx={{ width: itemInfoWidth }}
-              >
-                Color
-              </Typography>
-              <Typography>: {product.color}</Typography>
-            </Grid>
-            <Grid container item xs={6}>
-              <Typography
-                variant="body1"
-                gutterBottom
-                sx={{ width: itemInfoWidth }}
-              >
-                Fabric
-              </Typography>
-              <Typography>: {product.material}</Typography>
-            </Grid>
+          </Grid>
 
-            <Grid container item xs={7}>
-              <Typography
-                variant="body1"
-                gutterBottom
-                sx={{
-                  width: itemInfoWidth,
-                  fontSize: "1.1rem",
-                  fontWeight: "bold",
-                  mt: 4,
-                }}
-              >
-                Size
-              </Typography>
-              <Grid container item xs={12}>
-                {product.sizes.map((productSize) => {
-                  return (
-                    <Button
-                      key={productSize.size}
-                      label={productSize.size}
-                      variant="outlined"
-                      sx={{ m: 1 }}
-                      disabled={productSize.size_inventory <= 0}
-                    />
-                  );
-                })}
+          <Grid container item>
+            <Grid container item direction="column" spacing={1}>
+              <Grid container item>
+                <Typography
+                  variant="body1"
+                  gutterBottom
+                  sx={{ width: itemInfoWidth }}
+                >
+                  Style
+                </Typography>
+                <Typography>: {product.style}</Typography>
+              </Grid>
+              <Grid container item>
+                <Typography
+                  variant="body1"
+                  gutterBottom
+                  sx={{ width: itemInfoWidth }}
+                >
+                  Color
+                </Typography>
+                <Typography>: {product.color}</Typography>
+              </Grid>
+              <Grid container item>
+                <Typography
+                  variant="body1"
+                  gutterBottom
+                  sx={{ width: itemInfoWidth }}
+                >
+                  Fabric
+                </Typography>
+                <Typography>: {product.material}</Typography>
               </Grid>
             </Grid>
+          </Grid>
 
-            <Grid container item sx={{ mt: 4, mb: 2 }} gap={1}>
-              <Button
-                label="-"
-                variant="outlined"
-                sx={{ minHeight: 0, minWidth: 0 }}
-                onClick={decrementItemCount}
-              />
-              <TextField
-                variant="outlined"
-                value={numberOfItems}
-                size="small"
-                sx={{ width: "70px", "& input": { textAlign: "center" } }}
-              />
-              <Button
-                label="+"
-                variant="outlined"
-                sx={{ minHeight: 0, minWidth: 0 }}
-                onClick={incrementItemCount}
-              />
+          <Grid container item>
+            <Typography
+              variant="body1"
+              gutterBottom
+              sx={{
+                width: itemInfoWidth,
+                fontSize: "1.1rem",
+                fontWeight: "bold",
+                mt: 4,
+              }}
+            >
+              Size
+            </Typography>
+            <Grid container item xs={12} sx={{ gap: 2 }}>
+              {product.sizes.map((productSize) => {
+                return (
+                  <Button
+                    key={productSize.size}
+                    label={productSize.size}
+                    variant="outlined"
+                    disabled={productSize.size_inventory <= 0}
+                  />
+                );
+              })}
             </Grid>
+          </Grid>
 
-            <Grid container item sx={{ mt: 6 }}>
-              {product.inventory > 0 ? (
-                <Button label="Add to Cart" variant="outlined" />
-              ) : (
-                <Button label="Out of Stock" disabled variant="outlined" />
-              )}
-            </Grid>
-          </Box>
-        </Grid>
-        <Grid container sx={{ mt: 6 }}>
-          <Grid item sx={{ marginLeft: 10 }}>
-            <Typography variant="h6" component="h2">
-              Reviews
-            </Typography>
-            <Typography variant="subtitle1" component="h2">
-              Write comments here
-            </Typography>
-            <Typography variant="subtitle1" component="h2" sx={{ mt: 1 }}>
-              Please login to review
-            </Typography>
-            <Typography variant="subtitle2" component="h2" sx={{ mt: 2 }}>
-              There are no reviews for this product.
-            </Typography>
+          <Grid container item gap={2}>
+            <Button
+              label="-"
+              variant="outlined"
+              sx={{ minHeight: 0, minWidth: 0 }}
+              onClick={decrementItemCount}
+            />
+            <TextField
+              variant="outlined"
+              value={numberOfItems}
+              size="small"
+              sx={{ width: "70px", "& input": { textAlign: "center" } }}
+            />
+            <Button
+              label="+"
+              variant="outlined"
+              sx={{ minHeight: 0, minWidth: 0 }}
+              onClick={incrementItemCount}
+            />
+          </Grid>
+
+          <Grid container item>
+            {product.inventory > 0 ? (
+              <Button label="Add to Cart" variant="outlined" />
+            ) : (
+              <Button label="Out of Stock" disabled variant="outlined" />
+            )}
           </Grid>
         </Grid>
       </Grid>
-    </Grid >
+      <Grid container sx={{ mt: 6 }}>
+        <Grid item sx={{ marginLeft: 10 }}>
+          <Typography variant="h6" component="h2">
+            Reviews
+          </Typography>
+          <Typography variant="subtitle1" component="h2">
+            Write comments here
+          </Typography>
+          <Typography variant="subtitle1" component="h2" sx={{ mt: 1 }}>
+            Please login to review
+          </Typography>
+          <Typography variant="subtitle2" component="h2" sx={{ mt: 2 }}>
+            There are no reviews for this product.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
