2f70c2079244d645f09c07dd829dc0674e71ff9e
Shelly <shellybhattarai@gmail.com>
2024-04-27 17:07:57 +0545

    create users page and component

---
+++ src/app/admin/users/_components/index.tsx
@@ -0,0 +1,6 @@
+import Table from "@/components/Table";
+const UsersTable = () => {
+  return <Table />;
+};
+
+export default UsersTable;
---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+import UsersTable from "@/app/admin/users/_components";
+
+export default UsersTable;
