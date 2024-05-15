4b3f39669e52a4545bd4b3b4257a963f7720fdf5
Shelly <shellybhattarai@gmail.com>
2024-05-15 20:07:46 +0545

    edit background color

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -25,7 +25,7 @@ const Footer: React.FC = () => {
     <Box
       sx={{
         width: "100%",
-        bgcolor: (theme) => theme.palette.primary.main,
+        bgcolor: "background.paper",
         color: "text.secondary",
         borderTop: "1px solid",
         borderColor: "divider",
@@ -33,34 +33,29 @@ const Footer: React.FC = () => {
         pt: 1,
       }}
     >
-      <Container maxWidth={false}>
+       <Container maxWidth={false}>
         <Grid container spacing={2} justifyContent="space-between">
-          <Grid container item xs={12} sm={6} md={3} color="text.onPrimaryBg">
-            <Typography variant="h6" color="text.onPrimaryBg" gutterBottom>
+          <Grid container item xs={12} sm={6} md={3}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
               ThreadSwap
             </Typography>
             <Grid container item spacing={1}>
               <Grid container item direction="row">
                 <LocationOnIcon />
-                <Link href="#" color="text.onPrimaryBg" display="block" ml={1}>
+                <Link href="#" color="inherit" display="block" ml={1}>
                   Kathmandu, Nepal
                 </Link>
               </Grid>
               <Grid container item direction="row">
                 <LocalPhoneIcon />
-                <Link href="#" color="text.onPrimaryBg" display="block" ml={1}>
+                <Link href="#" color="inherit" display="block" ml={1}>
                   +977-9812345678
                 </Link>
               </Grid>
 
               <Grid container item direction="row">
                 <MailIcon />
-                <Link
-                  href="mailto:threadswap@gmail.com"
-                  color="text.onPrimaryBg"
-                  display="block"
-                  ml={1}
-                >
+                <Link href="#" color="inherit" display="block" ml={1}>
                   threadswap@gmail.com
                 </Link>
               </Grid>
@@ -70,62 +65,63 @@ const Footer: React.FC = () => {
           <Grid item xs={6} sm={3} md={2}>
             <Typography
               variant="subtitle1"
-              color="text.onPrimaryBg"
+              color="text.primary"
               sx={{ fontWeight: 700 }}
               gutterBottom
             >
               QUICK LINKS
             </Typography>
-            <Link
-              href="/products/instore/men/"
-              color="text.onPrimaryBg"
-              display="block"
-            >
+            <Link href="#" color="inherit" display="block">
               Men
             </Link>
-            <Link
-              href="/products/instore/women/"
-              color="text.onPrimaryBg"
-              display="block"
-            >
+            <Link href="#" color="inherit" display="block">
               Women
             </Link>
-            <Link
-              href="/products/instore/kids/"
-              color="text.onPrimaryBg"
-              display="block"
-            >
+            <Link href="#" color="inherit" display="block">
               Kids
             </Link>
-            <Link
-              href="/products/thrift/"
-              color="text.onPrimaryBg"
-              display="block"
-            >
+            <Link href="#" color="inherit" display="block">
               Thrift
+            </Link>
+            <Link href="#" color="inherit" display="block">
+              Contact
             </Link>
           </Grid>
 
           <Grid item xs={6} sm={3} md={2}>
             <Typography
               variant="subtitle1"
-              color="text.onPrimaryBg"
+              color="text.primary"
+              sx={{ fontWeight: 700 }}
+              gutterBottom
+            >
+              INFORMATION
+            </Typography>
+            <Link href="#" color="inherit" display="block">
+              Contact Us
+            </Link>
+          </Grid>
+
+          <Grid item xs={6} sm={3} md={2}>
+            <Typography
+              variant="subtitle1"
+              color="text.primary"
               sx={{ fontWeight: 700 }}
               gutterBottom
             >
               MY ACCOUNT
             </Typography>
-            <Link href="#" color="text.onPrimaryBg" display="block">
+            <Link href="#" color="inherit" display="block">
               My Profile
             </Link>
-            <Link href="#" color="text.onPrimaryBg" display="block">
+            <Link href="#" color="inherit" display="block">
               Order History
             </Link>
           </Grid>
         </Grid>
         <Typography
           variant="body2"
-          color="text.onPrimaryBg"
+          color="text.secondary"
           align="center"
           sx={{ pt: 4 }}
         >
