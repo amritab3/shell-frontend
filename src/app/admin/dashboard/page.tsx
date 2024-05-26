1e6413b02ec3a6bc8c37b1094b93abed315f6d7d
Shelly <shellybhattarai@gmail.com>
2024-05-26 08:44:29 +0545

    edit in dashboard page

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 
 import NotFound from "@/components/404";
+import { Grid, Typography } from "@mui/material";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -9,7 +10,18 @@ export const metadata: Metadata = {
 };
 
 const Dashboard = () => {
-  return <NotFound pageName="Dashboard" />;
+  return (
+    <Grid container sx={{ height: '60vh', justifyContent: 'center', alignItems: 'center',color: "primary.main" }}>
+      <Grid item>
+        <Typography component="h1" variant="h4" sx={{ fontWeight: 'bold' }}>
+          WELCOME TO THE DASHBOARD!
+        </Typography>
+      </Grid>
+    </Grid>
+
+
+
+  );
 };
 
 export default Dashboard;
