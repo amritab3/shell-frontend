7160e7442944b592fd102b9a3581fc95041fbd14
Shelly <shellybhattarai@gmail.com>
2024-05-26 09:42:22 +0545

    add button in dashboard page

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,26 +1,32 @@
+"use client";
+
 import React from "react";
 import type { Metadata } from "next";
+import { useRouter } from "next/navigation";
 
-import NotFound from "@/components/404";
 import { Grid, Typography } from "@mui/material";
 
-export const metadata: Metadata = {
-  title: "Dashboard",
-  description: "An eCommerce platform",
-};
+import Button from "@/components/Button";
+
 
 const Dashboard = () => {
+  const router = useRouter();
+
   return (
-    <Grid container sx={{ height: '60vh', justifyContent: 'center', alignItems: 'center',color: "primary.main" }}>
-      <Grid item>
+    <Grid container sx={{ height: '60vh', justifyContent: 'center', alignItems: 'center', color: "primary.main" }}>
+      <Grid item xs={12} sx={{ textAlign: 'center' }}> {/* Center content horizontally */}
         <Typography component="h1" variant="h4" sx={{ fontWeight: 'bold' }}>
           WELCOME TO THE DASHBOARD!
         </Typography>
       </Grid>
+      <Grid item xs={12} sx={{ textAlign: 'center' }}> {/* Center content horizontally */}
+        <Button
+          label="Go To Home"
+          variant="contained"
+          onClick={() => router.push("/")}
+        />
+      </Grid>
     </Grid>
-
-
-
   );
 };
 
